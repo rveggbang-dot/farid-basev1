7a313431d5b49c9f5f958f234e2638cc60b15194
Kingrve <rveggbang@gmail.com>
2025-11-21 23:50:03 +0800

    Add files via upload

---
+++ config/setting.js
@@ -0,0 +1,21 @@
+{
+  "name": "farid-base",
+  "version": "2.0.0",
+  "description": "Farid Base WhatsApp Bot with Baileys",
+  "main": "index.js",
+  "type": "commonjs",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "node index.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.5.0",
+    "qrcode-terminal": "^0.12.0",
+    "pino": "^8.15.0",
+    "axios": "^1.9.0",
+    "@hapi/boom": "^10.0.1"
+  },
+  "keywords": ["whatsapp", "bot", "baileys", "framework"],
+  "author": "Farid",
+  "license": "MIT"
+}
